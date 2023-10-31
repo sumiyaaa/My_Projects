6924e77a3933c6e520c2d15e0b1f6b03ff73f224
sumiya khan <96809687+sumiyaaa@users.noreply.github.com>
2023-10-31 21:35:28 +0500

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,26 @@
+function displayTime(){
+    var d = new Date();
+    var hour = d.getHours();
+    var min = d.getMinutes();
+    var sec = d.getSeconds();
+    var amOrPm = "AM";
+    if(hour >= 12)
+    {
+        amOrPm = "PM";
+    }
+    if(hour > 12)
+    {
+        hour = hour - 12;
+    }
+    if(hour < 10)
+    hour = "0" + hour;
+    if(min < 10)
+    min = "0" + min;
+    if(sec < 10)
+    min = "0" + sec;
+
+
+
+    document.getElementById("time").innerHTML = hour + ":" + min + ":" + sec + " " + amOrPm;
+   }
+   setInterval(displayTime , 1000)
